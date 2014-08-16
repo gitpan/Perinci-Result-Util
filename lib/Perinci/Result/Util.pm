82350532f525d38c1705497ddc214f4a34b88751
Steven Haryanto <stevenharyanto@gmail.com>
2014-08-16 02:22:08 +0000

    Import of SHARYANTO/Perinci-Result-Util-0.02 from CPAN.
    gitpan-cpan-distribution: Perinci-Result-Util
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Perinci-Result-Util-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Perinci/Result/Util.pm
+++ lib/Perinci/Result/Util.pm
@@ -10,8 +10,8 @@ our @EXPORT_OK = qw(
                        is_env_res
                );
 
-our $VERSION = '0.01'; # VERSION
-our $DATE = '2014-04-17'; # DATE
+our $VERSION = '0.02'; # VERSION
+our $DATE = '2014-08-16'; # DATE
 
 sub is_env_res {
     my $res = shift;
@@ -20,10 +20,14 @@ sub is_env_res {
     return 0 unless @$res <= 4;
 
     # check status
-    return 0 unless defined($res->[0]) && $res->[0] =~ /\A[0-9]{1,3}\z/;
+    return 0 unless defined($res->[0]) && $res->[0] =~ /\A[0-9]{1,3}\z/
+        && $res->[0] >= 100 && $res->[0] < 599;
 
     # check message
     return 0 unless !ref($res->[1]);
+    if (defined $res->[1]) {
+        return 0 unless $res->[1] =~ /[A-Za-z]/;
+    }
 
     # check result metadata
     return 0 if defined($res->[3]) && ref($res->[3]) ne 'HASH';
@@ -46,7 +50,7 @@ Perinci::Result::Util - Utilities related to enveloped result
 
 =head1 VERSION
 
-version 0.01
+This document describes version 0.02 of Perinci::Result::Util (from Perl distribution Perinci-Result-Util), released on 2014-08-16.
 
 =head1 SYNOPSIS
 
@@ -54,12 +58,13 @@ version 0.01
 
  say is_env_res([200, "OK"]); # 1
  say is_env_res("ok");        # 0
+ say is_env_res([1, 2, 3]);   # 0
 
 =head1 FUNCTIONS
 
 =head2 is_env_res($res) => BOOL
 
-Return true if C<$res> looks like enveloped result.
+Return true if C<$res> looks like enveloped result. It employs some heuristics.
 
 =head1 SEE ALSO
 
